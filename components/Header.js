80d12dec2c8a7419bb2d299d88bfb092b390667d
abhishal-ks <abhishalsharma2002@gmail.com>
2025-10-23 15:49:49 +0530

    Make the header bg blur

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='bg-white/95 px-5 py-2 flex items-center border-b border-[#ddd] sticky top-0 shadow-xs z-50'>
+    <header className='bg-white/70 backdrop-blur-md px-5 py-2 flex items-center border-b border-[#ddd] sticky top-0 shadow-xs z-50'>
         <img className='mr-4 h-14' src="PNG.png" alt="TipDoor Vendor" />
         <h1 className='font-bold text-2xl text-slate-800'>TipDoor - Vendor</h1>
     </header>
