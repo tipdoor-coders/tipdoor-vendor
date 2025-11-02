f60bee9bea4ad3ca3f2a13981ef7053c47e5f9a1
abhishal-ks <abhishalsharma2002@gmail.com>
2025-11-02 15:23:45 +0530

    Update padding in Entry component for improved layout

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Entry() {
     <>
       <div className="inset-0 -z-10 w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)] pb-36 max-md:h-screen flex flex-col justify-center">
 
-        <div className="mx-4 pt-9 pb-12 px-14 space-y-4 flex flex-col justify-center items-center text-center">
+        <div className="mx-4 pt-9 pb-12 px-16 space-y-4 flex flex-col justify-center items-center text-center">
           <div className="relative h-40 w-40">
             <Image
               src="/PNG.png"
